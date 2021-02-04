811db095a2786888d0ea1598bd046723903acde4
Micah Arvey <micaharvey@gmail.com>
2021-02-04 15:11:29 -0800

    buttons

--- crispy-chords/src/components/Stave.jsx
+++ crispy-chords/src/components/Stave.jsx
@@ -1,6 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
+import { Piano } from "@tonejs/piano";
+import { Button, Card } from "semantic-ui-react";
 import Vex from "vexflow";
 import "../styles/Stave.css";
+
+const noteToMidi = {
+  a: 69,
+  b: 71,
+  c: 60,
+  d: 62,
+  e: 64,
+  f: 65,
+  g: 67,
+  A: 69,
+  B: 71,
+  C: 60,
+  D: 62,
+  E: 64,
+  F: 65,
+  G: 67,
+};
 
 function Stave() {
   let note = "c";
@@ -8,7 +27,9 @@ function Stave() {
   let context;
   let stave;
   let group;
-  const [message, setMessage] = useState("Type the note on your keyboard");
+  let pianoLoaded = false;
+  let piano;
+  const message = "Select the correct note";
 
   const getRandomNote = () => {
     const noteNames = ["a", "b", "c", "d", "e", "f", "g"];
@@ -18,7 +39,9 @@ function Stave() {
   };
 
   const checkInput = (k) => {
-    if (k !== note) {
+    console.log(k);
+    console.log(VF, stave, context);
+    if (k.toLowerCase() !== note.toLowerCase()) {
       playIncorrectNoteMessage();
       return;
     }
@@ -45,11 +68,6 @@ function Stave() {
     context.closeGroup();
   };
 
-  const cleanupIntro = () => {
-    document.querySelector("#startButton").className = "hide";
-    document.querySelector(".message").className = "message";
-  };
-
   const initKeyboardCapture = () => {
     document.addEventListener("keydown", function (e) {
       switch (e.key) {
@@ -69,8 +87,34 @@ function Stave() {
     });
   };
 
+  const initPiano = () => {
+    // hide the start button
+    document.querySelector("#startButton").className = "hide";
+    document.querySelector(".loader").className = "loader";
+    window.requestAnimationFrame(function (time) {
+      window.requestAnimationFrame(function (time) {
+        document.querySelector(".loader").className = "loader loading";
+      });
+    });
+
+    piano = new Piano({
+      velocities: 5,
+    });
+
+    // Connect piano to speaker output
+    piano.toDestination();
+
+    piano.load().then(() => {
+      // console.log("Piano Loaded");
+      pianoLoaded = true;
+      document.querySelector(".loader").className = "loader hide";
+      initGameOnLoad();
+    });
+  };
+
   const initGameOnLoad = () => {
-    cleanupIntro();
+    document.querySelector(".message").className = "message";
+    document.querySelector(".buttonKeyboard").className = "buttonKeyboard";
     initKeyboardCapture();
 
     VF = Vex.Flow;
@@ -80,7 +124,7 @@ function Stave() {
     const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
     // Configure the rendering context.
-    renderer.resize(500, 500);
+    renderer.resize(200, 200);
     context = renderer.getContext();
     context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
@@ -103,12 +147,15 @@ function Stave() {
     context.closeGroup();
   };
 
-  // window.onload = (e) => {
-  //   initGameOnLoad();
-  // };
-
   function playCorrectNoteMessage() {
-    setMessage("Correct!");
+    if (pianoLoaded) {
+      // play the note and release in half a second
+      piano.keyDown({ midi: noteToMidi[note], velocity: 0.33 });
+      piano.keyUp({ midi: noteToMidi[note], time: "+0.5" });
+    }
+
+    // update the message and play the animation
+    // setMessage("Correct!");
     document.querySelector(".message").className = "message";
     window.requestAnimationFrame(function (time) {
       window.requestAnimationFrame(function (time) {
@@ -119,7 +166,7 @@ function Stave() {
   }
 
   function playIncorrectNoteMessage() {
-    setMessage("Try again");
+    // setMessage("Try again");
     document.querySelector(".message").className = "message";
     window.requestAnimationFrame(function (time) {
       window.requestAnimationFrame(function (time) {
@@ -129,21 +176,86 @@ function Stave() {
     });
   }
 
+  const buttonKeyboardClicked = (e, note) => {
+    e.preventDefault();
+    console.log(e, note);
+    checkInput(note);
+  };
+
   return (
     <div className="Stave">
       <h1>Name that note!</h1>
-      <button
+      <Button
         id="startButton"
         onClick={() => {
-          initGameOnLoad();
+          initPiano();
         }}
       >
         Click Here to begin
-      </button>
+      </Button>
       <h3 className="message hide" style={{ margin: "0 40px" }}>
         {message}
       </h3>
+      <div className="loader hide"></div>
       <div id="vexFlowElement"></div>
+      <div className="buttonKeyboard hide">
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            buttonKeyboardClicked(e, "a");
+          }}
+        >
+          A
+        </Button>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            buttonKeyboardClicked(e, "b");
+          }}
+        >
+          B
+        </Button>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            buttonKeyboardClicked(e, "c");
+          }}
+        >
+          C
+        </Button>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            buttonKeyboardClicked(e, "d");
+          }}
+        >
+          D
+        </Button>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            buttonKeyboardClicked(e, "e");
+          }}
+        >
+          E
+        </Button>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            buttonKeyboardClicked(e, "f");
+          }}
+        >
+          F
+        </Button>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            buttonKeyboardClicked(e, "g");
+          }}
+        >
+          G
+        </Button>
+      </div>
     </div>
   );
 }
